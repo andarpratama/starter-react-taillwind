6bf635400dfe27c98cfd26eacd47fb7e2b6a3e7b
andarpratama <master.amarta@gmail.com>
2021-12-04 19:49:56 +0700

    installation and setup

---
+++ src/pages/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Register extends React.Component {
+
+    componentDidMount(){
+        document.title = 'Register'
+    }
+
+    render(){
+        return(
+            <h1>Register</h1>
+        )
+    }
+}
+
+export default Register
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '547px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '768',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1024',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1680',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: '#ff4800'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
